1451fc684556f751c10f36a32981d803cf734512
Dawood ahmad <143737131+dawood078@users.noreply.github.com>
2025-12-29 21:36:44 +0530

    Add files via upload

---
+++ recurtion.java
@@ -0,0 +1,29 @@
+// public class recurtion {
+//     class Factorial {
+//     static int factorial(int n) {
+//         if (n == 0 || n == 1)  // Base case
+//             return 1;
+
+//         return n * factorial(n - 1); // Recursive call
+//     }
+
+//     public static void main(String[] args) {
+//         System.out.println(factorial(5));
+//     }
+// }
+
+// }
+
+// class Fibonacci {
+//     static int fib(int n) {
+//         if (n == 0) return 0;
+//         if (n == 1) return 1;
+
+//         return fib(n - 1) + fib(n - 2);
+//     }
+
+//     public static void main(String[] args) {
+//         System.out.println(fib(6));
+//     }
+// }
+
